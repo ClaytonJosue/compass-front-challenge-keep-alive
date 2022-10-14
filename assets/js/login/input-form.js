31f92cc9f39cb5521a6c04e3a1cc52612aeff268
ClaytonJosue <jogamesbr123@gmail.com>
2022-10-13 21:17:50 -0300

    script: input form animation

---
+++ assets/js/login/input-form.js
@@ -0,0 +1,31 @@
+const form = document.querySelector(".login__form");
+const userInput = form.querySelector("#user");
+const userPassword = form.querySelector("#user-password");
+
+const userIcon = form.querySelector("#user-icon");
+const passwordIcon = form.querySelector("#password-icon");
+
+let user = {
+	email: "admin@admin",
+	password: "admin"
+};
+
+userInput.addEventListener("input", userIconLeft);
+
+userPassword.addEventListener("input", passwordIconLeft);
+
+function userIconLeft() {
+	if (userInput.value.length > 0) {
+		userIcon.classList.add("input--active");
+	} else {
+		userIcon.classList.remove("input--active");
+	}
+}
+
+function passwordIconLeft() {
+	if (userPassword.value.length > 0) {
+		passwordIcon.classList.add("input--active");
+	} else {
+		passwordIcon.classList.remove("input--active");
+	}
+}
